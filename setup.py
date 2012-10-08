22727fae31f92c9e8faa390c53be06a85b99ddbe
Cameron Eure <turbinater@gmail.com>
2012-10-08 11:48:04 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 setup(
     name = 'murmur3',
-    version = '0.1',
+    version = '0.2',
     ext_modules = [
         Extension('murmur3', ['module.cpp', 'MurmurHash3.cpp'],),
         ],
